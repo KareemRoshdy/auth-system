2b7c238adc8a3951589d885059940c415c8f57a8
KareemRoshdy <karem22roshdy@gmail.com>
2024-08-21 16:12:34 +0300

    Update: Database_URL and migration

--- prisma/migrations/20240820133121_update_user/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `resetPasswordToke` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "resetPasswordToke",
-ADD COLUMN     "resetPasswordToken" TEXT;
--- prisma/migrations/20240821131123_initial/migration.sql
+++ prisma/migrations/20240821131123_initial/migration.sql
@@ -4,15 +4,19 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
     "isVerified" BOOLEAN NOT NULL DEFAULT false,
     "lastLogin" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "resetPasswordToke" TEXT NOT NULL,
-    "resetPasswordExpiresAt" TIMESTAMP(3) NOT NULL,
-    "verificationToke" TEXT NOT NULL,
-    "verificationTokeExpiresAt" TIMESTAMP(3) NOT NULL,
+    "resetPasswordToken" TEXT,
+    "resetPasswordExpiresAt" TIMESTAMP(3),
+    "verificationToken" TEXT,
+    "verificationTokenExpiresAt" TIMESTAMP(3),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_verificationToken_key" ON "User"("verificationToken");
