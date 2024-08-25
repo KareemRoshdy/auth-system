ffc0441906f303528b4018994f9f519ecd574829
KareemRoshdy <karem22roshdy@gmail.com>
2024-08-25 20:58:12 +0300

    Edit: FormHeading Style

--- src/components/FormHeading.tsx
+++ src/components/FormHeading.tsx
@@ -4,8 +4,10 @@ interface FormHeadingProps {
 
 const FormHeading = ({ label }: FormHeadingProps) => {
   return (
-    <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
-      {label}
+    <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-white bg-clip-text">
+      <span className="bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+        {label}
+      </span>
     </h2>
   );
 };
